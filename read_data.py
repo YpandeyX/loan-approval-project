e7c2de71275e973548996c70a787311356a51c46
YpandeyX <Pandityogesh0772@gmail.com>
2026-06-16 14:06:16 +0530

    Completed preprocessing and encoding

--- read_data.py
+++ read_data.py
@@ -2,6 +2,49 @@ import pandas as pd
 
 df = pd.read_csv(r"C:\Users\YOGESH PANDIT\OneDrive\Desktop\loan_approval\Loan_approval.csv")
 
-print(df.head())
-print(df.info())
 print(df.isnull().sum())
+
+df["Gender"] = df["Gender"].fillna(df["Gender"].mode()[0])
+df["Married"] = df["Married"].fillna(df["Married"].mode()[0])
+df["Dependents"] = df["Dependents"].fillna(df["Dependents"].mode()[0])
+df["Self_Employed"] = df["Self_Employed"].fillna(df["Self_Employed"].mode()[0])
+df["Property_Area"] = df["Property_Area"].fillna(df["Property_Area"].mode()[0])
+df["Credit_History"] = df["Credit_History"].fillna(df["Credit_History"].mode()[0])
+df["Loan_Amount_Term"] = df["Loan_Amount_Term"].fillna(df["Loan_Amount_Term"].median())
+
+print(df.isnull().sum())
+
+df["Gender"] = df["Gender"].map({"Male": 1, "Female": 0})
+df["Married"] = df["Married"].map({"Yes": 1, "No": 0})
+df["Education"] = df["Education"].replace({
+    "Graduate": 1,
+    "Not Graduate": 0,
+    "Grad": 1
+})
+df["Self_Employed"] = df["Self_Employed"].map({"Yes": 1, "No": 0})
+df["Loan_Status"] = df["Loan_Status"].map({"Y": 1, "N": 0})
+
+print(df.head())
+print(df.dtypes)
+print(df["Education"].unique())
+print(df["Education"].isnull().sum())
+
+
+df["Dependents"] = df["Dependents"].replace({
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3+": 3,
+    "four": 4
+})
+
+df["Property_Area"] = df["Property_Area"].replace({
+    "Rural": 0,
+    "Semiurban": 1,
+    "Urban": 2,
+    "Metro": 3
+})
+
+df = df.drop("Loan_ID", axis=1)
+
+print(df.dtypes)
--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 print("Hello")
+
+age = 21
+if age>=18:
+    print("Helllo My name eis Yogesh Pandey")
+
+for i in range(1,20,4):
+    print(i)
+
+
+i = 0
+while i<20:
+    print("while count is eligible",i)  
+    i+=1
+
+
